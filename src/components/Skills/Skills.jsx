64be38b67181033d2de2d5e5bf00e5f932ebdc3f
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-05-27 20:07:19 +0600

    skills added

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,8 +1,19 @@
 
 const Skills = () => {
     return (
-        <div>
-            <h1>skills</h1>
+        <div className="py-12 dark:bg-black">
+            <div className="flex justify-center items-center space-x-10 w-5/6 mx-auto ">
+                <img className="w-48 h-40" src="https://i.ibb.co/3stWtXy/html5-logo-31813.png" alt="" />
+                <img className="w-48 h-40 bg-white" src="https://i.ibb.co/HYtQPTW/pngegg-1.png" alt="" />
+                
+                <img className="w-48" src="https://i.ibb.co/M1frk1Y/mongo.png" alt="" />
+                <img className="w-48" src="https://i.ibb.co/LhVHHLB/express.png" alt="" />
+                <img className="w-48 animate-spin-slow" src="https://i.ibb.co/c8t2WJK/react.png" alt="" />
+                <img className="w-48" src="https://i.ibb.co/Dbh6b41/node.png" alt="" />
+                <img className="w-48 h-40" src="https://i.ibb.co/7rXJ6Zt/pngegg.png" alt="" />
+
+                <img className="w-48 h-40" src="https://i.ibb.co/zQMQQ6y/pngegg-2.png" alt="" />
+            </div>
         </div>
     );
 };
